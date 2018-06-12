c7f80b2687c714dfde2750628226d0fc36256ed1
liguanghua <1339879576@qq.com>
2018-06-12 15:13:46 +0800

    新增首页专题、专题图片添加缩放和过渡效果（IE9只支持缩放不支持过渡）、首页页脚、滚动时固定搜索框、完善公共样式文件

--- js/index.js
+++ js/index.js
@@ -10,3 +10,24 @@ sinput.onfocus=function(){
 sinput.onblur=function(){
     sresult.style.display="none";
 }
+
+var sinput2=document.getElementById("i-lscsi-input2");
+var sresult2=document.getElementById("i-lscsi-sresult2");
+sinput2.onfocus=function(){
+    sresult2.style.display="block";
+}
+sinput2.onblur=function(){
+    sresult2.style.display="none";
+}
+
+//页面滚动到第二屏时显示搜索框
+var fixedSearch=document.getElementById("i-fixed-search-wrapper");
+document.onscroll=function(){
+    var scrollTop=document.body.scrollTop || document.documentElement.scrollTop;
+    var clientHeight=document.body.clientHeight || document.documentElement.clientHeight;
+    if(scrollTop>=200){
+        fixedSearch.style.display="block";
+    }else{
+        fixedSearch.style.display="none";
+    }
+}
